d32b8a249090c17abd9853ccec50a88a8057549f
marcos-framinan <framinanvideiramarcos@gmail.com>
2025-11-11 09:55:03 +0100

    Add files via upload

---
+++ ejercicios_2.py
@@ -0,0 +1,22 @@
+def doble_factorial(n):
+    "Calcula el doble factorial de n."
+    if n < 0:
+        raise ValueError("El número no puede ser negativo.")
+    if n == 0 or n == 1:
+        return 1
+    
+    sol = 1
+    for i in range(n, 0, -2):
+        sol *= i
+    return sol
+
+try:
+    numero = int(input("Introduce un número entero: "))
+    sol = doble_factorial(numero)
+    print(f"El doble factorial de {numero} es: {sol}")
+
+except ValueError as e:
+    print(f"Error: {e}")
+
+except Exception as e:
+    print(f"Ocurrió un error inesperado: {e}")
